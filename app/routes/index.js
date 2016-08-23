2ac41a7215c6ac7714ad450331e3091a55b3b367
Julie David <julie.angela.david@gmail.com>
2016-08-23 02:23:57 -0400

    move blog posts to model in the Route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,40 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model() {
+		return [
+			{
+				title:'GEMconf 2016 day 1',
+				tagline:'',
+				body:'Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.',
+				date:'Tuesday, August 24, 2016',
+				image:'/assets/images/panel-2-3.jpg',
+				alt:'Triangle GEMconf #emberjs'
+			},
+			{
+				title:'GEMconf 2016 day 1',
+				tagline:'',
+				body:'Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.',
+				date:'Tuesday, August 24, 2016',
+				image:'/assets/images/panel-2-3.jpg',
+				alt:'Triangle GEMconf #emberjs'
+			},
+			{
+				title:'GEMconf 2016 day 1',
+				tagline:'',
+				body:'Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.',
+				date:'Tuesday, August 24, 2016',
+				image:'/assets/images/panel-2-3.jpg',
+				alt:'Triangle GEMconf #emberjs'
+			},
+			{
+				title:'GEMconf 2016 day 1',
+				tagline:'',
+				body:'Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.',
+				date:'Tuesday, August 24, 2016',
+				image:'/assets/images/panel-2-3.jpg',
+				alt:'Triangle GEMconf #emberjs'
+			},
+		];
+	}
 });
