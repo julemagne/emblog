a81041a65a5047d37830ea8a3030d3f7db92f06c
Julie David <julie.angela.david@gmail.com>
2016-09-08 11:26:42 -0400

    use ember data with mirage fake GET request on index and posts archive pages to retrieve blog posts

---
+++ app/models/post.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+	title: DS.attr(),
+	tagline: DS.attr(),
+	body: DS.attr(),
+	date: DS.attr(),
+	image: DS.attr(),
+	alt: DS.attr()
+});
