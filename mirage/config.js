f2be43f15d5b303e232d12c03497b628583580a5
Julie David <julie.angela.david@gmail.com>
2016-09-08 11:20:49 -0400

    install mirage fake REST server to prepare for ember data to replace hard-coded model in routes

---
+++ mirage/config.js
@@ -0,0 +1,80 @@
+  // These comments are here to help you get started. Feel free to delete them.
+
+  /*
+    Config (with defaults).
+
+    Note: these only affect routes defined *after* them!
+  */
+
+  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
+  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+
+  /*
+    Shorthand cheatsheet:
+
+    this.get('/posts');
+    this.post('/posts');
+    this.get('/posts/:id');
+    this.put('/posts/:id'); // or this.patch
+    this.del('/posts/:id');
+
+    http://www.ember-cli-mirage.com/docs/v0.2.x/shorthands/
+  */
+
+export default function() {
+  this.get('/posts', function() {
+    return {
+      data: [
+        {
+          type: 'posts',
+          id: 1,
+          attributes: {
+            title:'GEMconf 2016 day 1',
+            tagline:'',
+            body:'Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.',
+            date:'Tuesday, August 24, 2016',
+            image:'/assets/images/panel-2-3.jpg',
+            alt:'Triangle GEMconf #emberjs'
+          }
+        },
+        {
+          type: 'posts',
+          id: 2,
+          attributes: {
+            title:'GEMconf 2016 day 1',
+            tagline:'',
+            body:'Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.',
+            date:'Tuesday, August 24, 2016',
+            image:'/assets/images/panel-2-3.jpg',
+            alt:'Triangle GEMconf #emberjs'
+          }
+        },
+        {
+          type: 'posts',
+          id: 3,
+          attributes: {
+            title:'GEMconf 2016 day 1',
+            tagline:'',
+            body:'Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.',
+            date:'Tuesday, August 24, 2016',
+            image:'/assets/images/panel-2-3.jpg',
+            alt:'Triangle GEMconf #emberjs'
+          }
+        },
+        {
+          type: 'posts',
+          id: 4,
+          attributes: {
+            title:'GEMconf 2016 day 1',
+            tagline:'',
+            body:'Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.',
+            date:'Tuesday, August 24, 2016',
+            image:'/assets/images/panel-2-3.jpg',
+            alt:'Triangle GEMconf #emberjs'
+          }
+        }
+      ]
+    };
+  });
+}
