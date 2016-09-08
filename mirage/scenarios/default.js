685df509942217cb581e8313e33e94ee2264b8d0
Julie David <julie.angela.david@gmail.com>
2016-09-08 18:31:31 -0400

    have 4 different entries for fake blog posts

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -4,7 +4,7 @@ export default function(server) {
     id: 1,
     title:'GEMconf 2016 day 1',
     tagline:'',
-    body:'Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.',
+    body:'Lorem ipsum dolor sit amet, consectetur adipirm dolor sit amet, consectetur adipiscing vehicula id em ipsum dolor sit amet, consectetur adipiscing vehiscing vehicula id nulla dignissim dapibus ultrices.',
     date:'Tuesday, August 24, 2016',
     image:'/assets/images/panel-2-3.jpg',
     alt:'Triangle GEMconf #emberjs'
@@ -22,21 +22,21 @@ export default function(server) {
 
   server.create('post', {
     id: 3,
-    title:'GEMconf 2016 day 1',
+    title:'The Iron Yard Durham Back End Engineering: Programming Bootcamp Experience',
     tagline:'',
-    body:'Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.',
-    date:'Tuesday, August 24, 2016',
-    image:'/assets/images/panel-2-3.jpg',
+    body:'Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapiit amet, consectetur adipibus ultrices.',
+    date:'Wednesday, September 7, 2016',
+    image:'http://julemagne.github.io/portfolio/images/fulls/arttrack.png',
     alt:'Triangle GEMconf #emberjs'
   });
 
   server.create('post', {
     id: 4,
-    title:'GEMconf 2016 day 1',
+    title:'Show and Tell \'Emblog\' at September \'16 Ember Meetup',
     tagline:'',
-    body:'Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla dignissim dapibus ultrices.',
-    date:'Tuesday, August 24, 2016',
-    image:'/assets/images/panel-2-3.jpg',
+    body:'Lorem ipslor situm dolor lor sitsit amet, consectetlor situr adipiscing vehilor sitcula id nulla dilor sitgnissim dapibus ultrices.',
+    date:'Thursday, September 8, 2016',
+    image:'/assets/images/emblog.png',
     alt:'Triangle GEMconf #emberjs'
   });
 
