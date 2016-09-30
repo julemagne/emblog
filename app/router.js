3aa032e4edd16bc29cde8ca5129f3f3c5ae1e11a
Julie David <julie.angela.david@gmail.com>
2016-09-30 03:47:58 -0400

    add page-not-found wildcard route for 404s

--- app/router.js
+++ app/router.js
@@ -11,6 +11,7 @@ Router.map(function() {
   this.route('post', { path: '/post/:post_id' });
   this.route('about');
   this.route('contact');
+  this.route('page-not-found', { path: '/*wildcard' });
 });
 
 export default Router;
